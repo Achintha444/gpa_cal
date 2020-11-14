654cf7063ef2d5efb8e4fe702638c7ffd989cfc9
Achinths Isuru <achinthaisuru.17@cse.mrt.ac.lk>
2020-11-14 22:55:25 +0530

    splash screen added

--- lib/ui/splash_screen_page/page/splash_screen_page.dart
+++ lib/ui/splash_screen_page/page/splash_screen_page.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SplashScreenPage extends StatelessWidget {
-
   const SplashScreenPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: this.key,
-      body: Container(),
+      
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 216,
+              child: Image(
+                image: AssetImage('graphics/logo.png'),
+              ),
+            ),
+            SizedBox(height: 100),
+            Container(
+              height: 216,
+              child: SvgPicture.asset(
+                'graphics/cap.svg',
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
