a2d1146186a10bdd6c00a8853ee0b1516244c9de
Achinths Isuru <achinthaisuru.17@cse.mrt.ac.lk>
2020-12-09 15:03:23 +0530

    anroid screen roation set to potrait only

--- lib/root.dart
+++ lib/root.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'theme/project_theme.dart';
 import 'ui/splash_form_page/splash_form_exports.dart';
@@ -7,6 +8,10 @@ import 'ui/splash_screen_page/splash_screen_exports.dart';
 class GpaCal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       title: 'GPA CAL',
       theme: projectTheme,
